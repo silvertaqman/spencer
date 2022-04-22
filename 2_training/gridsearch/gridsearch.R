dc69938959a614ffb333c73a822b92a1dd865d70
silvertaqman <ajcedeno@espe.edu.ec>
2022-04-21 20:08:51 -0500

    rbf and lin svm optimized through grdisearch at the HPC. Contour and scatter plots generated

--- 2_training/gridsearch/gridsearch.R
+++ 2_training/gridsearch/gridsearch.R
@@ -3,8 +3,9 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(stringr)
-# Modifications to files in ./preplotting.gz
+library(ggpubr)
+# RBF
+# Modificate files in ./preplotting.gz for exploratory analysis
 # Loading data and add a run column, then drop na values
 #surface <- read_csv("params.csv") %>%
 #	add_row(read_csv("params1.csv")) %>%
@@ -14,15 +15,14 @@ library(stringr)
 # Export
 #write_csv(surface, "parameters.csv")
 # Plots: sequential explanation to delimite best parameters
-surface <- read_csv("parameters.csv.gz") %>%
+surfacerbf <- read_csv("parameters.csv.gz") %>%
 	select(param_C, param_gamma, mean_test_score, run) %>%
 	mutate(accuracy = cut(mean_test_score, breaks = 10))	
  
-surface %>%	
-	filter(run != 'third', param_gamma <100 ) %>%
+contourA<-surfacerbf %>%	
+	filter(run == 'second', param_gamma <100 ) %>%
 	ggplot(aes(x=param_gamma, y=param_C, z=mean_test_score))+
 	geom_contour_filled(bins = 20)+
-	facet_wrap(.~run, scales="free", ncol = 3)+
   # New fill and legend title for number of tracks per region
   scale_fill_gradientn(
     "Accuracy",
@@ -47,13 +47,14 @@ surface %>%
   # Make the guide for the fill discrete
   guides(
     fill = guide_colorsteps(
-      barwidth = 50, barheight = .75, title.position = "top", title.hjust = .5
+      barwidth = 32, barheight = .75, title.position = "top", title.hjust = .5
     )
   )
-	
+
+
 ggsave('firstsecond.pdf', dpi=300)
 
-surface %>% 
+surfacerbf %>% 
 	filter(run == 'third') %>%
 	ggplot(aes(x=param_gamma, y=param_C, z=mean_test_score))+
 	geom_contour_filled(bins=10)+
@@ -92,7 +93,7 @@ ggsave('third.pdf', dpi=300)
 # 3D scatterplot
 library(gg3D)
 
-surface %>%
+scatterA<-surfacerbf %>%
 	filter(run == 'second', param_gamma < 100, param_gamma > 0, ) %>% 
 	ggplot(
 		aes(x=param_gamma, 
@@ -121,7 +122,7 @@ surface %>%
   # Make the guide for the fill discrete
   guides(
     colour = guide_colorsteps(
-      barwidth = 50, barheight = 0.5, title.position = "left", title.hjust = .5
+      barwidth = 20, barheight = 0.5, title.position = "left", title.hjust = .5
     )
   )
 
@@ -129,7 +130,7 @@ ggsave('scatter.pdf', dpi=300)
 
 # Dynamic chart
 library(plotly)
-surface %>% plot_ly(
+surfacerbf %>% plot_ly(
 	x=~param_gamma, 
 	y=~param_C, 
 	z=~mean_test_score,
@@ -137,3 +138,113 @@ surface %>% plot_ly(
 	type="scatter3d", 
 	mode="markers",
 	colors = c("#5E1E5B","#A62231","#F26F7E","#E95420","#F89B0F"))
+	
+# LINEAR
+surfacelin <- read_csv("lin.csv.gz") %>%
+	select(param_C, param_gamma, mean_test_score) %>%
+	mutate(accuracy = cut(mean_test_score, breaks = 10))	
+ 
+contourB<-surfacelin %>%	
+	ggplot(aes(x=param_gamma, y=param_C, z=mean_test_score))+
+	geom_contour_filled(bins = 20)+
+  # New fill and legend title for number of tracks per region
+  scale_fill_gradientn(
+    "Accuracy",
+     colours = c("#5E1E5B","#A62231","#F26F7E","#E95420","#F89B0F"),
+     super = metR::ScaleDiscretised)+
+  theme(
+    # Remove axis ticks and text
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    # Use gray text for the region names
+    axis.text.x = element_text(color = "gray12", size = 12),
+    axis.text.y = element_text(color = "gray12", size = 12),
+    # Move the legend to the bottom
+    legend.position = "bottom",
+    # Set default color and font family for the text
+    text = element_text(color = "gray12"),
+    # Make the background white and remove extra grid lines
+    panel.background = element_rect(fill = "white", color = "white"),
+    panel.grid = element_blank(),
+    panel.grid.major.x = element_blank()
+  )+
+  # Make the guide for the fill discrete
+  guides(
+    fill = guide_colorsteps(
+      barwidth = 32, barheight = .75, title.position = "top", title.hjust = .5
+    )
+  )
+	
+ggsave('contour.pdf', dpi=300)
+
+# GRAPHICAL OPTIONS
+
+# 3D scatterplot
+library(gg3D)
+
+scatterB<-surfacelin %>% 
+	ggplot(
+		aes(x=param_gamma, 
+				y=param_C, 
+				z=mean_test_score, 
+				colour = accuracy))+
+	axes_3D(theta=90,phi=60)+
+	stat_3D(theta=90, phi=60, geom="point", size = 2)+
+  scale_colour_gradientn(
+    "",
+     colours = c("#5E1E5B","#A62231","#F26F7E","#E95420","#F89B0F"),
+     super = metR::ScaleDiscretised)+
+  theme(
+    # Use gray text for the region names
+    axis.text.x = element_text(color = "gray12", size = 12),
+    axis.text.y = element_text(color = "gray12", size = 12),
+    # Move the legend to the bottom
+    legend.position = "bottom",
+    # Set default color and font family for the text
+    text = element_text(color = "gray12"),
+    # Make the background white and remove extra grid lines
+    panel.background = element_rect(fill = "white", color = "white"),
+    panel.grid = element_blank(),
+    panel.grid.major.x = element_blank()
+  )+
+  # Make the guide for the fill discrete
+  guides(
+    colour = guide_colorsteps(
+      barwidth = 20, barheight = 0.5, title.position = "left", title.hjust = .5
+    )
+  )
+
+ggsave('scatter.lin.pdf', dpi=300)
+
+# Dynamic chart
+library(plotly)
+surfacelin %>% plot_ly(
+	x=~param_gamma, 
+	y=~param_C, 
+	z=~mean_test_score,
+	color =~mean_test_score,
+	type="scatter3d", 
+	mode="markers",
+	colors = c("#5E1E5B","#A62231","#F26F7E","#E95420","#F89B0F"))
+	
+# Merge two plots
+con <- ggarrange(contourA, 
+					contourB,
+					labels = c("A", "B"),
+					ncol = 2, 
+					nrow = 1)
+sca <- ggarrange(scatterA, 
+					scatterB,
+					labels = c("A", "B"),
+					ncol = 2, 
+					nrow = 1)
+ggsave("contours.pdf",
+				con, 
+				width = 3600, 
+				height = 1800, 
+				units = "px")
+ggsave("scatterplot.pdf",
+				sca, 
+				width = 3200, 
+				height = 1600, 
+				units = "px")
--- 2_training/gridsearch/gridsearch.py
+++ 2_training/gridsearch/gridsearch.py
@@ -19,7 +19,7 @@ X, Xt, y, yt = tts(bc_input,bc_output,random_state=74)
 # 1) gamma y C de 0.0001 a 1000 becomes 0.91
 # 2) gamma de 0.0001 a 1 y C de 1 a 1000 becomes 0.9484
 # 3) gamma de 0.3 a 0.33 y C de 20 a 40 becomes 0.9484
-# 4) Se estabiliza entre C: 25, gamma = 0.303
+# 4) Se estabiliza entre C: 25, gamma = 0.303 
 gamma = [i/100 for i in range(1,101)]
 C = [i for i in range(1,101)]
 kernel = ['linear']
@@ -43,14 +43,5 @@ Hacer un scatter plot con los mejores accuracy (como superficie)
 Se buscaron los parámetros con la funcion gridsearchcv en sklearn para svm: discretiza el espacio de parametros y prueba (C: de 1 a 20) y (gamma: de 0.001 hasta 1 con salto de 0.1)
 '''
 # Grid search en busca de los parametros del SVM lineal
-# the process goes like this:
-# best_score of 0.811 with C=100, gamma=0.001
-# best_score of 0.796 with C=5, gamma=0.01
-# best score of 0.811 with C=45, gmma = 0.001
-# best_score of 0.814 with C=7, gamma=0.0044 (0.829 con Xt y yt)
-# best score of 0.8137 with C=21, gamma = 0.002
-# best score of 0.817 with C = 9, gamma = 0.002
-# best score of 0.817 with C = 9, gamma = 0.00315 (0.8205 con Xt y yt)
-# best score of 0.819 with C = 11, gamma = 0.003  (0.8205 con Xt y yt)
-# best score of 0.8193374 with C = 10, gamma = 0.0028  (0.82051 con Xt y yt)
-# Parece que existen varios maximos locales
+# 100*100 mesh from gamma: 0.01 to 1, and C from 1 to 100
+# generates an accuracy of 0. with C: 3, gamma= 0.8
