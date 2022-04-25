0d3985b2c383f8e202ca5eee043abd74235988e0
silvertaqman <ajcedeno@espe.edu.ec>
2022-04-25 16:20:30 -0500

    gridsearch over 4 models, models and metrics generated, validation achieved

--- 2_training/3_Train.py
+++ 2_training/3_Train.py
@@ -34,6 +34,7 @@ results = pd.DataFrame(gs.cv_results_)
 Features = list(results.columns)
 params = pd.DataFrame(results,columns=Features)
 params.to_csv('rbf.csv', index=False)
+joblib.dump(svmrbf, "./models/bc_svmrbf.pkl")
 
 # Linear
 # 2) # 100*100 mesh from gamma: 0.01 to 1, and C from 1 to 100
@@ -43,28 +44,24 @@ param_grid = {
 	'C': [i for i in range(1,101)],
 	'kernel': ['linear']
 }
-param_grid = {'C': C,'gamma': gamma, 'kernel':kernel}
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
 gs = GridSearchCV(SVC(), param_grid, cv=5).fit(X,y) # takes a lot of time
 # training with best parameters
-svmrbf = SVC(**gs.best_params_).fit(X,y)
+svmlin = SVC(**gs.best_params_).fit(X,y)
 # Export metrics
 results = pd.DataFrame(gs.cv_results_)
 Features = list(results.columns)
 params = pd.DataFrame(results,columns=Features)
 params.to_csv('linear.csv', index=False)
+joblib.dump(svmlin, "./models/bc_svmlin.pkl")
 
 # Logistic regression
 # 3) generates an accuracy of 0.91 with C: 3, gamma= 0.8
-penalty = 
-C = 
-solver = 
 param_grid = {
-	'C': np.logspace(-3,3,7),
-	'penalty': ['l2', 'none'], 
-	'solver': ['newton-cg', 'lbfgs', 'sag'], 
-	multiclass = 'ovr'
+        'C': np.logspace(-3,4,50),
+        'penalty': ['l2', 'none'],
+        'solver': ['newton-cg', 'lbfgs', 'sag'],
+        'random_state': [74],
+        'max_iter': [50000]
 }
 from sklearn.linear_model import LogisticRegression as LR
 gs = GridSearchCV(LR(), param_grid, cv=5).fit(X,y)
@@ -75,23 +72,23 @@ results = pd.DataFrame(gs.cv_results_)
 Features = list(results.columns)
 params = pd.DataFrame(results,columns=Features)
 params.to_csv('logistic.csv', index=False)
+joblib.dump(lr, "./models/bc_lr.pkl")
 # A second option could be added with the second best lr params
 
 # Multilayer perceptron
 # 4) 
-
 param_grid = {
-	'hidden_layer_sizes': [ (20,), (50,50,50), (50,100,50), (100,)],
-	'activation': ['identity', 'logistic', 'tanh', 'relu'],
-	'solver': ['sgd', 'adam'],
-	'alpha': [0.0001, 0.001, 0.05],
-	'learning_rate_init': np.logspace(-6,0,7),
-	'random_state' = 74, 
-	'max_iter'= 50000,
-	'shuffle'= False
+        'hidden_layer_sizes': [ (20,), (100,), (50,50,50), (50,100,50)],
+        'activation': ['logistic', 'tanh', 'relu'],
+        'solver': ['sgd', 'adam'],
+        'alpha': [0.0001, 0.05],
+        'learning_rate_init': np.logspace(-3,0,4),
+        'random_state': [74],
+        'max_iter': [50000],
+        'shuffle': [False]
 }
 from sklearn.neural_network import MLPClassifier as MLPC
-gs = GridSearchCV(MLPC(), param_grid, cv=5).fit(X,y)
+gs = GridSearchCV(MLPC(), param_grid, cv=3).fit(X,y)
 # training with best parameters
 mlp = MLPC(**gs.best_params_).fit(X,y)
 # Export metrics
@@ -99,15 +96,10 @@ results = pd.DataFrame(gs.cv_results_)
 Features = list(results.columns)
 params = pd.DataFrame(results,columns=Features)
 params.to_csv('mlp.csv', index=False)
+joblib.dump(mlp, "./models/bc_mlp.pkl")
 '''
 Problema dual: maximizas el valor de la ganancia
 '''
-# Export models
-joblib.dump(svmlin, "./models/bc_svmlin.pkl")
-joblib.dump(svmrbf, "./models/bc_svmrbf.pkl")
-joblib.dump(lr, "./models/bc_lr.pkl")
-joblib.dump(mlp, "./models/bc_mlp.pkl")
-
 # Validation
 models = [svmlin, svmrbf, lr, mlp]
 # K-fold Validation
